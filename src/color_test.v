17404b271de403601763f02be29eb3c9311f8f3c
Ferdinand Prantl <prantlf@gmail.com>
2023-08-11 15:25:28 +0200

    ci: Expect disable colours on CI

--- src/color_test.v
+++ src/color_test.v
@@ -3,7 +3,7 @@ module debug
 import os { getenv }
 
 fn test_get_color() {
-	expected := if has_env('NO_COLOR') {
+	expected := if has_env('NO_COLOR') || has_env('CI') {
 		''
 	} else {
 		if getenv('FORCE_COLOR') == '1' {
