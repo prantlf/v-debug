a31735b4ed17cb5663f0d7e6a0997c8f5464b347
Ferdinand Prantl <prantlf@gmail.com>
2023-08-11 15:46:02 +0200

    Revert "ci: Expect disabled colours on CI"
    This reverts commit c53d73d136882dbbe34cee2ea4af86a87cceeb89.

--- src/color_test.v
+++ src/color_test.v
@@ -3,10 +3,10 @@ module debug
 import os { getenv }
 
 fn test_get_color() {
-	expected := if has_env('NO_COLOR') {
+	expected := if has_env('NO_COLOR') || has_env('CI') {
 		''
 	} else {
-		if getenv('FORCE_COLOR') == '1' || has_env('CI') {
+		if getenv('FORCE_COLOR') == '1' {
 			'4'
 		} else {
 			'8;5;43'
