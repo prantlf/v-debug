fa75541868a98a90809998e063c9c74a33d0b1eb
Ferdinand Prantl <prantlf@gmail.com>
2023-08-11 15:54:25 +0200

    ci: Expect disabled colours on CI

--- src/color_test.v
+++ src/color_test.v
@@ -3,11 +3,13 @@ module debug
 import os { getenv }
 
 fn test_get_color() {
-	expected := if has_env('NO_COLOR') || has_env('CI') {
+	expected := if has_env('NO_COLOR') {
 		''
 	} else {
 		if getenv('FORCE_COLOR') == '1' {
 			'4'
+		} else if has_env('CI') {
+			''
 		} else {
 			'8;5;43'
 		}
