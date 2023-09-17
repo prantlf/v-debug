28bc588ee8e8f2e0f303a6d9b87c9aedcfe0d60f
Ferdinand Prantl <prantlf@gmail.com>
2023-09-17 03:26:34 +0200

    fix: Remove the call to IsWindows10OrGreater
    VersionHelpers.h is not available on github and the linker failed withtout it.

--- src/color.v
+++ src/color.v
@@ -4,10 +4,10 @@ import math { abs }
 import os { getenv, getenv_opt }
 import term { can_show_color_on_stderr }
 
-$if windows {
-	#include <windows.h>
-}
-fn C.IsWindows10OrGreater() int
+// $if windows {
+// 	#include <windows.h>
+// }
+// fn C.IsWindows10OrGreater() int
 
 const few_colors = ['6', '2', '3', '4', '5', '1']!
 
@@ -145,11 +145,12 @@ fn detect_colors() int {
 	}
 
 	$if windows {
-		return if C.IsWindows10OrGreater() != 0 {
-			2
-		} else {
-			1
-		}
+		return 2
+		// return if C.IsWindows10OrGreater() != 0 {
+		// 	2
+		// } else {
+		// 	1
+		// }
 	}
 
 	if has_env('CI') {
